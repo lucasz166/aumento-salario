a56282db61ec50caa650e8ba27428d0883cd5eda
Lucas Gabriel <lucasgaguiar.rocha@gmail.com>
2021-09-13 22:23:45 -0300

    My first commit

---
+++ src/main/java/SALARIO.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+
+public class SALARIO {
+    
+public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        float salario=0.0f, aumento=0.0f, salarioNovo=0.0f;
+        
+       System.out.println("Digite salario: ");
+       salario = entrada.nextFloat();
+         
+        aumento = salario*10/100;
+        salarioNovo=salario+aumento;
+        
+        System.out.printf("O salario novo é %.2f \n", salarioNovo);
+        
+    }
+
+}
